ee78b78dfdd1ab2f35ef93d225c3207044a52ea5
Eric Snell <eric_snell21@yahoo.com>
2016-06-20 14:06:05 -0400

    instructions added

---
+++ js/script.js
@@ -0,0 +1,46 @@
+/* Game asks questions to determine your taste and identifies ingredients
+   to suit those tastes. */
+
+// Create Constructor Functions for Questions, Ingredients, and the Pantry
+
+// Use those Constructors to create Objects representing the Questions, Ingredients,
+// and pantry
+
+// The UI will ask a Question and provide an input for the answer
+
+// When the user submits choice, you should build a preferences Object (if the user
+// chooses wants a strong drink then this should be recording in the Object)
+
+// Ingredient category and Question category
+// create cunstroctors that create category
+
+// createDrink would randomize what ingredients to pick based on answers
+
+$(document).ready(function(event){
+
+	// store question objects
+	var questions = {
+		strong: "Do you like your drinks strong?",
+		salty:
+	}
+
+	// contains all of the available ingredients
+	var ingredients = {
+		strong: ["rum", "whiskey", "gin"],
+		salty: ["olive",]
+	}
+
+	var pantry = {
+		"rum": 10, 
+		"whiskey": 5
+	}
+
+
+
+
+
+/*--------- FUNCTIONS ----------*/
+
+
+
+});
